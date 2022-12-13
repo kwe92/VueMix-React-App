a1e469701d96cacb72d3d325274cc0cd4d1b5e49
Kweayon  Clark <Kweayon@gmail.com>
2022-12-13 16:48:20 -0600

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Movies from "./components/Movies";
+import { Route, Routes } from "react-router-dom";
 
 const App = (props) => (
   <div className="App">
