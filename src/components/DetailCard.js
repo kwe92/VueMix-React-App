4b6d1af4741782fb0beb96487e198fab2cbbe3ed
Kweayon  Clark <Kweayon@gmail.com>
2022-12-16 17:06:50 -0600

    Refactor

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -1,60 +1,44 @@
 import React from "react";
 import { StyledCard } from "../styles/styled_components/styles";
 import { AiFillStar } from "react-icons/ai";
+import {
+  StyledCardContentImage,
+  StyledCardContentRow,
+  StyledCardContentTitle,
+  StyledCardContentDate,
+  StyledCardContentRating,
+  StyledCardContentP,
+  StyledCardContentP2,
+} from "../styles/styled_components/card_styles/cardStyles";
 
-//TODO: Remove inline styles
-//TODO: Fix coloring of text by mode
-const DetailCard = (props) => (
-  <StyledCard>
-    <img src={props.image} style={{ height: "18vw", width: "30vw" }} />
-    <div style={{ padding: "0rem 1rem", textAlign: "center" }}>
-      <h2 style={{ color: props.isLightMode ? "black" : "white" }}>
-        {props.title}
-      </h2>
-      <p
-        style={{
-          color: props.isLightMode ? "black" : "white",
-          margin: "0rem 0rem 0.25rem 0rem",
-        }}
-      >
-        {props.releaseDate}
-      </p>
-      <div
-        style={{
-          display: "flex",
-          // backgroundColor: "red",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <p
-          style={{
-            paddingRight: "0.25rem",
-            margin: 0,
-            color: props.isLightMode ? "black" : "white",
-          }}
-        >
-          Rating:
-        </p>
-        <AiFillStar style={{ color: "gold" }}></AiFillStar>
-        <p
-          style={{
-            paddingLeft: "0.25rem",
-            margin: 0,
-            color: props.isLightMode ? "black" : "white",
-          }}
-        >
-          {props.rating}
-        </p>
-      </div>
-    </div>
-
-    <div style={{ padding: "0px 1rem 0.5rem" }}>
-      <p style={{ color: props.isLightMode ? "black" : "white" }}>
-        {props.overview}
-      </p>
-    </div>
-  </StyledCard>
-);
+const DetailCard = (props) => {
+  return (
+    <StyledCard>
+      <StyledCardContentImage src={props.image} />
+      <StyledCardContentRow>
+        <StyledCardContentTitle isLightMode={props.isLightMode}>
+          {props.object.original_title}
+        </StyledCardContentTitle>
+        <StyledCardContentDate isLightMode={props.isLightMode}>
+          {props.object.release_date}
+        </StyledCardContentDate>
+        <StyledCardContentRating>
+          <StyledCardContentP isLightMode={props.isLightMode}>
+            Rating:
+          </StyledCardContentP>
+          <AiFillStar
+            style={{ color: "gold", paddingRight: "0.25rem" }}
+          ></AiFillStar>
+          <StyledCardContentP isLightMode={props.isLightMode}>
+            {parseFloat(props.object.vote_average).toFixed(2)}
+          </StyledCardContentP>
+        </StyledCardContentRating>
+      </StyledCardContentRow>
+      <StyledCardContentP2 isLightMode={props.isLightMode}>
+        {props.object.overview}
+      </StyledCardContentP2>
+    </StyledCard>
+  );
+};
 
 export default DetailCard;
