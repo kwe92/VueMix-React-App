cdafcc13174a611a93927cb8d11d07a07996246c
Kweayon  Clark <Kweayon@gmail.com>
2022-12-15 21:18:14 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Movies from "./components/Movies";
-import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
-const App = (props) => (
-  <div className="App">
-    <NavBar />
-  </div>
-);
+const App = (props) => {
+  // TODO: Extract into its own custom hook to keep code DRY
+  const [isLightMode, setIsLightMode] = useState(true);
+
+  const modeHandler = (mode) => {
+    setIsLightMode(mode);
+  };
+
+  return (
+    <div
+      className="App"
+      style={{
+        //TODO: Change background color based on mode light or dark
+        backgroundColor: !isLightMode && "grey",
+        position: "absolute",
+        left: 0,
+        width: "100%",
+        height: "auto",
+      }}
+    >
+      <NavBar getMode={modeHandler} />
+      {/* <Movies></Movies> */}
+    </div>
+  );
+};
 
 export default App;
