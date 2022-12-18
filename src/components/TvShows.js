5776cc52311968d9aa98e2fefec9537c722b9d6e
Kweayon  Clark <Kweayon@gmail.com>
2022-12-18 17:49:21 -0600

    Added filter

--- src/components/TvShows.js
+++ src/components/TvShows.js
@@ -1,13 +1,13 @@
-import { useTvShowsState } from "../Hooks/apiStateHooks";
+import { useTvShowsState } from "../hooks/apiStateHooks";
 import BaseLayoutGrid from "./BaseLayoutGrid";
 
 const TvShows = (props) => {
   const [tvShows, setTvShowsState] = useTvShowsState();
-
   return (
     <BaseLayoutGrid
       stateParams={[tvShows, setTvShowsState]}
       mode={props.mode}
+      filterVal={props.filterVal}
     ></BaseLayoutGrid>
   );
 };
