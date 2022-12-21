829f5aefe5c96f84c03885c48df71de966137b4e
Kweayon  Clark <Kweayon@gmail.com>
2022-12-20 20:38:00 -0600

    Update

--- src/Hooks/apiStateHooks.js
+++ src/Hooks/apiStateHooks.js
@@ -22,22 +22,21 @@ const useMoviesState = () => {
     movieCall();
   };
 
+  const result = moviesData.map((movie) =>
+    dataModel(
+      movie.original_title,
+      movie.overview,
+      movie.release_date,
+      movie.vote_average,
+      movie.vote_count,
+      movie.poster_path,
+      movie.backdrop_path
+    )
+  );
+
   useEffect(getMovies, []);
   //todo: add secondary poster
-  return [
-    moviesData.map((movie) =>
-      dataModel(
-        movie.original_title,
-        movie.overview,
-        movie.release_date,
-        movie.vote_average,
-        movie.vote_count,
-        movie.poster_path,
-        movie.backdrop_path
-      )
-    ),
-    SetMoviesData,
-  ];
+  return [result, SetMoviesData];
 };
 
 // TV Shows API Call
@@ -56,56 +55,53 @@ const useTvShowsState = () => {
     tvShow();
   };
 
+  const result = tvShowsData.map((tvshow) =>
+    dataModel(
+      tvshow.original_name,
+      tvshow.overview,
+      tvshow.first_air_date,
+      tvshow.vote_average,
+      tvshow.vote_count,
+      tvshow.poster_path,
+      tvshow.backdrop_path
+    )
+  );
+
   useEffect(getTvShow, []);
-  return [
-    tvShowsData.map((tvshow) =>
-      dataModel(
-        tvshow.original_name,
-        tvshow.overview,
-        tvshow.first_air_date,
-        tvshow.vote_average,
-        tvshow.vote_count,
-        tvshow.poster_path,
-        tvshow.backdrop_path
-      )
-    ),
-    ,
-    SetTvShowsData,
-  ];
+  return [result, SetTvShowsData];
 };
 
 // Trending API Call
 // TV Shows API Call
 const useTrendingState = () => {
   const api = `https://api.themoviedb.org/3/trending/all/week?api_key=${apikey}`;
-  const [trendingData, setTrendingState] = useState([]);
+  const [trendingData, setTrendingDataState] = useState([]);
 
   const trending = async () => {
     const data = await axios.get(api);
     const results = data.data.results;
     console.log("Results Trending", results);
-    setTrendingState(results);
+    setTrendingDataState(results);
   };
 
   const getTrending = () => {
     trending();
   };
 
+  const result = trendingData.map((trending) =>
+    dataModel(
+      trending.original_title,
+      trending.overview,
+      trending.release_date,
+      trending.vote_average,
+      trending.vote_count,
+      trending.poster_path,
+      trending.backdrop_path
+    )
+  );
+
   useEffect(getTrending, []);
-  return [
-    trendingData.map((trending) =>
-      dataModel(
-        trending.original_title,
-        trending.overview,
-        trending.release_date,
-        trending.vote_average,
-        trending.vote_count,
-        trending.poster_path,
-        trending.backdrop_path
-      )
-    ),
-    setTrendingState,
-  ];
+  return [result, setTrendingDataState];
 };
 
 export default useMoviesState;
