3fbb366974811af900e50579e323c5534c949976
Kweayon  Clark <Kweayon@gmail.com>
2022-12-20 20:35:57 -0600

    Const Data For Packages

---
+++ src/constants/packageData.js
@@ -0,0 +1,12 @@
+const basicPackage = [
+  "Unlimited movies and tvshows",
+  "Stream on mobile devices",
+  "Cancel at any time",
+  "First month completely free",
+];
+
+const standardPackage = [...basicPackage, "HD movies and tv shows"];
+
+const premiumPackage = [...standardPackage, "4k movies and tv shows"];
+
+export const packages = [basicPackage, standardPackage, premiumPackage];
