1aeb2c076024ff61238427416fd9af87e603fd44
Kweayon  Clark <Kweayon@gmail.com>
2022-12-20 20:37:12 -0600

    Refactor

--- src/trailers/TrailersMovies.js
+++ src/trailers/TrailersMovies.js
@@ -2,57 +2,73 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import ReactPlayer from "react-player";
 import movieTrailer from "movie-trailer";
-import { VideoModal } from "../styles/video_modal/videoModalStyle";
+import * as style from "../styles/video_modal/videoModalStyle";
+import sadFace from "../assets/sadface_nbg.png";
+import styled from "styled-components";
 
 // TODO: Handle errors due to no inital movie value and no trailer found along with any onther error caught by the browser
-// TODO: Fix previous video state showing shortly before newstate is rendered
+// TODO: Slim component down and get rid of duplicaet lazy code!
+
 export const TrailerMovies = (props) => {
-  const [video, setVideo] = useState();
+  const [video, setVideo] = useState([]);
   //https://youtu.be/sa9l-dTv9Gk
   const [videoURL, setVideoURL] = useState([""]);
   const [showModal, setShowModal] = props.showModal;
 
-  if (props.title) {
-    movieTrailer(props.title).then((res) => {
+  useEffect(() => {
+    setVideo(props.title);
+    movieTrailer(video).then((res) => {
       setVideoURL(res);
     });
-  }
+  }, [video]);
 
-  // const handleSearch = () => {
-  //   console.log("from handle search");
-  //   setVideo(props.title);
-  //   movieTrailer(props.title).then((res) => {
-  //     setVideoURL(res);
-  //   });
-  // };
+  const handleModalClose = () => {
+    setVideoURL("");
+    setShowModal(false);
+  };
 
-  // useEffect(() => {
-  //   handleSearch();
-  // }, [video]);
+  const closeButton = (
+    <div onClick={handleModalClose}>
+      <span>&times;</span>
+    </div>
+  );
 
-  console.log("from trailer movies", props.title);
+  console.log("from trailer movies", videoURL);
 
-  if (props.title) {
-    return (
-      <VideoModal className="modal" display={showModal}>
-        <VideoModal className="modal-content" isLightMode={props.isLightMode}>
-          <div
-            onClick={() => {
-              setShowModal(false);
-            }}
-          >
-            <span>&times;</span>
-          </div>
-          <div style={{ height: "90%" }}>
-            <ReactPlayer
-              url={videoURL}
-              controls={true}
-              width={"100%"}
-              height={"100%"}
-            />
-          </div>
-        </VideoModal>
-      </VideoModal>
-    );
-  }
+  return (
+    <style.VideoModal className="modal" display={showModal}>
+      <style.VideoModal
+        className="modal-content"
+        isLightMode={props.isLightMode}
+      >
+        {videoURL ? (
+          <>
+            {closeButton}
+            <div style={{ height: "90%" }} id={"player"}>
+              <ReactPlayer
+                url={videoURL}
+                controls={true}
+                width={"100%"}
+                height={"100%"}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            {closeButton}
+            <style.ErrorWrapper>
+              <style.ErrorMessage isLightMode={props.isLightMode}>
+                No Video Found
+              </style.ErrorMessage>
+              <style.ErrorTitle isLightMode={props.isLightMode}>
+                Title: {video}
+              </style.ErrorTitle>
+
+              <img src={sadFace} />
+            </style.ErrorWrapper>
+          </>
+        )}
+      </style.VideoModal>
+    </style.VideoModal>
+  );
 };
