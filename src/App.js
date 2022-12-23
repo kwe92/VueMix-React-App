d35efaa8ad9ef0483b91be83092702146deca129
Kweayon  Clark <Kweayon@gmail.com>
2022-12-23 12:53:22 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import NavBar from "./components/NavBar";
 import { StyledAppWrapper } from "./styles/styled_components/styles";
 import { useState } from "react";
-import { TrailerMovies } from "./trailers/TrailersMovies";
+import { TrailerMovies } from "./components/TrailersMovies";
+import { useMode } from "./hooks/modeHook";
 const App = (props) => {
   // TODO: Extract into its own custom hook to keep code DRY
-  const [isLightMode, setIsLightMode] = useState(false);
+  // TODO: Finish custom app bar
+  // TODO: Create a soruce of truth theme file
+  // TODO: Add sign in page for google auth
+  // TODO: Add Trending dropdown?
+
+  const [isLightMode, setIsLightMode] = useMode();
   const [title, setTitle] = useState("");
   const [showModal, setShowModal] = useState(false);
 
