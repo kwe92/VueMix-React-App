7b527a86187a156d7de9221d10344be23b6f3c37
Kweayon  Clark <Kweayon@gmail.com>
2022-12-23 12:49:59 -0600

    Update

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from "react";
-import { packages } from "../constants/packageData";
+import React from "react";
+import { packages, bundles } from "../constants/packageData";
 import * as style from "../styles/styled_components/pricePageStyles";
+// TODO: Shorten this component to keep code DRY
 // Nested arrays
 const wrappedPackages = packages.map((subType) =>
   subType.map((type) => <li key={type.replaceAll(" ", "")}>{type}</li>)
@@ -9,47 +10,45 @@ const wrappedPackages = packages.map((subType) =>
 console.log("wrapped packages: ", wrappedPackages);
 
 const buttonText = "Subscribe Now";
+const subBy = "/Month";
 
 const Pricing = (props) => {
+  const bundlesList = bundles.map((bundle) => {
+    return (
+      //TODO: Fix this div
+      //TODO: Should be its own component
+      <div
+        style={{
+          background: "white",
+          borderRadius: "20px",
+          width: "30vw",
+          height: "42vw",
+        }}
+      >
+        <style.SubscribeCard
+          isLightMode={props.mode}
+          key={bundle.name + bundle.index}
+        >
+          <h1 className="header"> {bundle.name} </h1>
+          <p className="price">
+            {bundle.price} <span>{subBy}</span>
+          </p>
+          {/* UL SHOULD BE ALIGNED CENTER!! */}
+          <ul style={{ paddingLeft: "2rem", paddingRight: "1rem" }}>
+            {wrappedPackages[bundle.index]}
+          </ul>
+          <style.SubscribeButton type="button">
+            {buttonText}
+          </style.SubscribeButton>
+        </style.SubscribeCard>
+      </div>
+    );
+  });
+
+  console.log("bundles list", bundlesList);
+
   return (
-    <style.SubscribeContentWrapper>
-      {
-        <>
-          <style.SubscribeCard isLightMode={props.mode}>
-            <h1 className="header"> Basic </h1>
-            <p className="price">
-              $7.99 <span>/Month</span>
-            </p>
-            <ul>{wrappedPackages[0]}</ul>
-            <style.SubscribeButton type="button">
-              {buttonText}
-            </style.SubscribeButton>
-          </style.SubscribeCard>
-
-          <style.SubscribeCard isLightMode={props.mode}>
-            <h1 className="header"> Standard </h1>
-            <p className="price">
-              $12.99 <span>/Month</span>
-            </p>
-            <ul>{wrappedPackages[1]}</ul>
-            <style.SubscribeButton type="button">
-              {buttonText}
-            </style.SubscribeButton>
-          </style.SubscribeCard>
-
-          <style.SubscribeCard isLightMode={props.mode}>
-            <h1 className="header"> Premium </h1>
-            <p className="price">
-              $18.99 <span>/Month</span>
-            </p>
-            <ul>{wrappedPackages[2]}</ul>
-            <style.SubscribeButton type="button">
-              {buttonText}
-            </style.SubscribeButton>
-          </style.SubscribeCard>
-        </>
-      }
-    </style.SubscribeContentWrapper>
+    <style.SubscribeContentWrapper>{bundlesList}</style.SubscribeContentWrapper>
   );
 };
 
