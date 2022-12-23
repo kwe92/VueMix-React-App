2b86d9bba32aeaae3eee983a6ca01fd258ecd52f
Kweayon  Clark <Kweayon@gmail.com>
2022-12-23 12:51:28 -0600

    Package Data

--- src/constants/packageData.js
+++ src/constants/packageData.js
@@ -10,3 +10,9 @@ const standardPackage = [...basicPackage, "HD movies and tv shows"];
 const premiumPackage = [...standardPackage, "4k movies and tv shows"];
 
 export const packages = [basicPackage, standardPackage, premiumPackage];
+
+export const bundles = [
+  { name: "Basic", price: "$7.99", index: 0 },
+  { name: "Standard", price: "$12.99", index: 1 },
+  { name: "Premium", price: "$18.99", index: 2 },
+];
