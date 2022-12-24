c27bfc6239872367da0f2482c9915be614508cbb
Kweayon  Clark <Kweayon@gmail.com>
2022-12-24 15:49:17 -0600

    Refactor

--- src/components/BaseLayoutGrid.js
+++ src/components/BaseLayoutGrid.js
@@ -4,62 +4,50 @@ import {
   StyledWrapper,
   StyledImageDisappear,
   StyledUlCards,
+  ContentWrapper,
 } from "../styles/styled_components/styles";
+import { CustomElements } from "../constants/CustomElements";
 
 const BaseLayoutGrid = (props) => {
   // destructure state and setState
-  const [data, setDataState] = props.stateParams;
+  const [data, setDataState] = props.state;
   let filteredData;
 
-  // If props.filterVal is not empty
-  if (props.filterVal) {
-    filteredData = data.filter((ele) =>
-      isMatchingTitle(ele.original_title, props.filterVal)
-    );
-  } else {
-    filteredData = data;
-  }
+  //  Filter data based on title containing user input
+  props.filterVal
+    ? (filteredData = data.filter((ele) =>
+        isMatchingTitle(ele.original_title, props.filterVal)
+      ))
+    : (filteredData = data);
 
-  // Create an array of elements wrapped in <li> tags
-  const movieList = filteredData.map((ele) => {
-    return (
-      <div style={{ position: "relative" }} key={ele.original_title}>
-        <li style={{ listStyleType: "none" }}>
-          <StyledWrapper>
-            <StyledImageDisappear
-              src={`https://image.tmdb.org/t/p/w500/${
-                ele.poster_path === null ? ele.backdrop_path : ele.poster_path
-              }`}
-            />
-            <DetailCard
-              image={
-                ele.backdrop_path === null
-                  ? `https://image.tmdb.org/t/p/w500/${ele.poster_path}`
-                  : `https://image.tmdb.org/t/p/w500/${ele.backdrop_path}`
-              }
-              isLightMode={props.mode}
-              object={ele}
-              getTitle={props.getTitle}
-            ></DetailCard>
-          </StyledWrapper>
-        </li>
-      </div>
-    );
-  });
+  // conentList:
+  //  - an array of images on top of cards wrapped in <li> tags
+
+  const conentList = filteredData.map((ele) => (
+    <ContentWrapper key={ele.original_title}>
+      <CustomElements.ListItem>
+        <StyledWrapper>
+          <StyledImageDisappear
+            src={`https://image.tmdb.org/t/p/w500/${
+              ele.poster_path === null ? ele.backdrop_path : ele.poster_path
+            }`}
+          />
+          <DetailCard
+            image={
+              ele.backdrop_path === null
+                ? `https://image.tmdb.org/t/p/w500/${ele.poster_path}`
+                : `https://image.tmdb.org/t/p/w500/${ele.backdrop_path}`
+            }
+            isLightMode={props.isLightMode}
+            object={ele}
+            getTitle={props.getTitle}
+          ></DetailCard>
+        </StyledWrapper>
+      </CustomElements.ListItem>
+    </ContentWrapper>
+  ));
   return (
-    // TODO: Should be its own styled component
-
-    // TODO: Remove this div?? What was it for?? | it made sense when i put it in
-    // <div
-    //   style={{
-    //     background: props.mode ? "white" : "#121212",
-    //     position: "absolute",
-    //     height: "100%",
-    //     width: "100%",
-    //   }}
-    // >
-    <StyledUlCards mode={props.mode}>{movieList}</StyledUlCards>
-    // </div>
+    <StyledUlCards isLightMode={props.isLightMode}>{conentList}</StyledUlCards>
   );
 };
 
