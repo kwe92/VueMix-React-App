7abe2a665331c64e84d2fe10a5ba57d1d59bff95
Kweayon  Clark <Kweayon@gmail.com>
2022-12-24 15:50:05 -0600

    Refactor

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,47 +1,45 @@
 import React from "react";
 import { packages, bundles } from "../constants/packageData";
 import * as style from "../styles/styled_components/pricePageStyles";
-// TODO: Shorten this component to keep code DRY
 // Nested arrays
 const wrappedPackages = packages.map((subType) =>
   subType.map((type) => <li key={type.replaceAll(" ", "")}>{type}</li>)
 );
-
-console.log("wrapped packages: ", wrappedPackages);
 
 const buttonText = "Subscribe Now";
 const subBy = "/Month";
 
 const Pricing = (props) => {
   const bundlesList = bundles.map((bundle) => {
+    const CardTopSection = (
+      <>
+        <h1 className="header"> {bundle.name} </h1>
+        <p className="price">
+          {bundle.price} <span>{subBy}</span>
+        </p>
+      </>
+    );
+
+    const CardBullets = (
+      <ul style={{ paddingLeft: "2rem", paddingRight: "1rem" }}>
+        {wrappedPackages[bundle.index]}
+      </ul>
+    );
+
+    const CardButton = (
+      <style.SubscribeButton type="button">{buttonText}</style.SubscribeButton>
+    );
     return (
-      //TODO: Fix this div
-      //TODO: Should be its own component
-      <div
-        style={{
-          background: "white",
-          borderRadius: "20px",
-          width: "30vw",
-          height: "42vw",
-        }}
-      >
+      <style.CardBlankCanvas>
         <style.SubscribeCard
-          isLightMode={props.mode}
+          isLightMode={props.isLightMode}
           key={bundle.name + bundle.index}
         >
-          <h1 className="header"> {bundle.name} </h1>
-          <p className="price">
-            {bundle.price} <span>{subBy}</span>
-          </p>
-          {/* UL SHOULD BE ALIGNED CENTER!! */}
-          <ul style={{ paddingLeft: "2rem", paddingRight: "1rem" }}>
-            {wrappedPackages[bundle.index]}
-          </ul>
-          <style.SubscribeButton type="button">
-            {buttonText}
-          </style.SubscribeButton>
+          {CardTopSection}
+          {CardBullets}
+          {CardButton}
         </style.SubscribeCard>
-      </div>
+      </style.CardBlankCanvas>
     );
   });
 
