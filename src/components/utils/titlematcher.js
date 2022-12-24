561ecdf676d2079548e4617233deffb5fd127d56
Kweayon  Clark <Kweayon@gmail.com>
2022-12-24 15:48:55 -0600

    Rfactor

--- src/components/utils/titlematcher.js
+++ src/components/utils/titlematcher.js
@@ -1,8 +1,5 @@
 // A utility function that returns as boolen value if one string is included in another
+// if the string is empty return false
 
-export const isMatchingTitle = (ele, val) => {
-  // if the string is empty return false
-  if (!ele) return false;
-
-  return ele.toLowerCase().includes(val.toLowerCase());
-};
+export const isMatchingTitle = (ele, val) =>
+  !ele ? false : ele.toLowerCase().includes(val.toLowerCase());
