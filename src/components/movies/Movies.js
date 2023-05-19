27f1acb749e527113fd5738a4213e1aa0d965b55
Kwe <Kweayon@gmail.com>
2023-05-19 17:38:26 -0500

    basic page button layout

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,17 +1,60 @@
 import React from "react";
 import useMoviesState from "../../hooks/apiStateHooks";
 import BaseLayoutGrid from "../base/BaseLayoutGrid";
+import styled from "styled-components";
 
 const Movies = (props) => {
   const [movies, setMoviesState] = useMoviesState();
   return (
-    <BaseLayoutGrid
-      state={[movies, setMoviesState]}
-      isLightMode={props.isLightMode}
-      filterVal={props.filterVal}
-      getTitle={props.getTitle}
-    ></BaseLayoutGrid>
+    <>
+      <BaseLayoutGrid
+        state={[movies, setMoviesState]}
+        isLightMode={props.isLightMode}
+        filterVal={props.filterVal}
+        getTitle={props.getTitle}
+      ></BaseLayoutGrid>
+      <PageButtons />
+    </>
   );
 };
+
+const PageButtons = ({}) => (
+  <PageButtonContainer>
+    <PageButton type="button">
+      <p>Back</p>
+    </PageButton>
+    <PageButton type="button">
+      <p>Next</p>
+    </PageButton>
+  </PageButtonContainer>
+);
+
+const PageButtonContainer = styled.div`
+  display: flex;
+  width: 50%;
+  height: 14rem;
+  background-color: grey;
+  align-items: center;
+  justify-content: space-evenly;
+
+  @media screen and (max-width: 800px) {
+    width: 60%;
+  }
+`;
+
+const PageButton = styled.button`
+  border-radius: 3.125rem;
+  width: 16rem;
+  height: 4rem;
+  border: none;
+  &: hover {
+    background: orange;
+    color: white;
+  }
+
+  @media screen and (max-width: 1024px) {
+    width: 12rem;
+  }
+`;
 
 export default Movies;
