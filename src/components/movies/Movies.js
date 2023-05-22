003c618f48d092c10252a8f517b873f6c937c039
Kwe <Kweayon@gmail.com>
2023-05-22 18:06:45 -0500

    refact: removed PageButton && PageButtonContainer into a shared module

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import useMoviesState from "../../hooks/apiStateHooks";
 import BaseLayoutGrid from "../base/BaseLayoutGrid";
-import styled from "styled-components";
+import { PageButton, PageButtonContainer } from "../shared/PageButtons";
 
 const Movies = (props) => {
   const [pageNumber, setPageNumber] = useState(1);
@@ -42,38 +42,5 @@ const PageButtons = ({ setPage, prevPage }) => (
     </PageButton>
   </PageButtonContainer>
 );
-// TODO: Make Responsive to smaller devices
-
-const PageButtonContainer = styled.div`
-  display: flex;
-  width: 45%;
-  padding: 2rem 0 2rem 0;
-  background-color: grey;
-  align-items: center;
-  justify-content: space-evenly;
-
-  @media screen and (max-width: 800px) {
-    width: 60%;
-  }
-
-  @media screen and (max-width: 450px) {
-    width: 100%;
-  }
-`;
-
-const PageButton = styled.button`
-  border-radius: 3.125rem;
-  width: 16rem;
-  height: 4rem;
-  border: none;
-  &: hover {
-    background: orange;
-    color: white;
-  }
-
-  @media screen and (max-width: 1024px) {
-    width: 12rem;
-  }
-`;
 
 export default Movies;
